2edbaf92691911a9281eba959851c753631d0041
疯狂的狮子Li <15040126243@163.com>
2026-03-19 10:43:43 +0800

    update 翻译模块升级jackson3

--- ruoyi-common/ruoyi-common-translation/src/main/java/org/dromara/common/translation/core/handler/TranslationBeanSerializerModifier.java
+++ ruoyi-common/ruoyi-common-translation/src/main/java/org/dromara/common/translation/core/handler/TranslationBeanSerializerModifier.java
@@ -1,9 +1,9 @@
 package org.dromara.common.translation.core.handler;
 
-import com.fasterxml.jackson.databind.BeanDescription;
-import com.fasterxml.jackson.databind.SerializationConfig;
-import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
-import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
+import tools.jackson.databind.BeanDescription;
+import tools.jackson.databind.SerializationConfig;
+import tools.jackson.databind.ser.BeanPropertyWriter;
+import tools.jackson.databind.ser.ValueSerializerModifier;
 
 import java.util.List;
 
@@ -12,10 +12,18 @@ import java.util.List;
  *
  * @author Lion Li
  */
-public class TranslationBeanSerializerModifier extends BeanSerializerModifier {
+public class TranslationBeanSerializerModifier extends ValueSerializerModifier {
 
+    /**
+     * 为翻译字段补充空值序列化器，确保字段值为 {@code null} 时仍能走翻译处理链。
+     *
+     * @param config         当前序列化配置
+     * @param beanDesc       Bean 描述提供者
+     * @param beanProperties 当前 Bean 的属性写入器列表
+     * @return 调整后的属性写入器列表
+     */
     @Override
-    public List<BeanPropertyWriter> changeProperties(SerializationConfig config, BeanDescription beanDesc,
+    public List<BeanPropertyWriter> changeProperties(SerializationConfig config, BeanDescription.Supplier beanDesc,
                                                      List<BeanPropertyWriter> beanProperties) {
         for (BeanPropertyWriter writer : beanProperties) {
             // 如果序列化器为 TranslationHandler 的话 将 Null 值也交给他处理
@@ -23,7 +31,7 @@ public class TranslationBeanSerializerModifier extends BeanSerializerModifier {
                 writer.assignNullSerializer(serializer);
             }
         }
-        return beanProperties;
+        return super.changeProperties(config, beanDesc, beanProperties);
     }
 
 }
