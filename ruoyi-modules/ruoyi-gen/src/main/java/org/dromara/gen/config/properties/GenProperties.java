52652b7e38b3a165c09cfaa10f398d2dd473e492
疯狂的狮子Li <15040126243@163.com>
2026-04-17 18:25:52 +0800

    update 重构 增强代码生成器各项功能 update 自动类型判断可根据不同数据库精确识别 对应的java类型 update 优化java字段名与数据库名不匹配则增加别名注解 update 增加 是否导出 是否状态切换 是否组合唯一校验 是否排序调整 和树结构相关字段等功能选择 update 优化自定义路径导出 导出全部代码 update 删除无用主子表相关代码

--- ruoyi-modules/ruoyi-gen/src/main/java/org/dromara/gen/config/properties/GenProperties.java
+++ ruoyi-modules/ruoyi-gen/src/main/java/org/dromara/gen/config/properties/GenProperties.java
@@ -17,6 +17,7 @@ import java.nio.charset.StandardCharsets;
 @Data
 @Component
 @ConfigurationProperties(prefix = "gen")
+@PropertySource(value = "classpath:generator.yml", factory = YmlPropertySourceFactory.class)
 public class GenProperties {
 
     /**
