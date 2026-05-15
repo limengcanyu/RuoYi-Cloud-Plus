87e770ebe62d54d1587b77372d0b3ba8b29d50a9
疯狂的狮子Li <15040126243@163.com>
2026-05-15 16:40:06 +0800

    update 增加join工具 简化join业务代码写法

--- ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/mapper/FlwUserMapper.java
+++ ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/mapper/FlwUserMapper.java
@@ -1,24 +1,8 @@
 package org.dromara.workflow.mapper;
 
-import cn.hutool.core.collection.CollUtil;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.github.yulichang.base.MPJBaseMapper;
-import com.github.yulichang.toolkit.JoinWrappers;
-import com.github.yulichang.wrapper.MPJLambdaWrapper;
-import org.dromara.common.core.utils.StringUtils;
 import org.dromara.common.mybatis.core.mapper.BaseMapperPlus;
-import org.dromara.warm.flow.orm.entity.FlowDefinition;
-import org.dromara.warm.flow.orm.entity.FlowHisTask;
-import org.dromara.warm.flow.orm.entity.FlowInstance;
 import org.dromara.warm.flow.orm.entity.FlowUser;
-import org.dromara.workflow.domain.FlowInstanceBizExt;
-import org.dromara.workflow.domain.bo.FlowTaskBo;
-import org.dromara.workflow.domain.vo.FlowTaskVo;
-
-import java.util.List;
-import java.util.Map;
-
-import static org.dromara.workflow.common.constant.FlowConstant.NOT_DELETED;
 
 /**
  * 流程用户查询 Mapper
